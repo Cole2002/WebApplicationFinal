83529ae192031ed3f24b697820211a0798376c2a
Cole2002 <164812620+Cole2002@users.noreply.github.com>
2024-04-19 13:32:01 -0400

    Add project files.

---
+++ WebApplicationFinal/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationFinal.Models;
+
+namespace WebApplicationFinal
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<Hobby> Hobbies { get; set; }
+        public DbSet<BreakfastFood> BreakfastFoods { get; set; }
+        public DbSet<FavoriteBook> FavoriteBooks { get; set; }
+    }
+}
+
+
+
---
+++ WebApplicationFinal/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationFinal.Models;
+
+namespace WebApplicationFinal
+{
+    public class YourDbContext : DbContext
+    {
+        public YourDbContext(DbContextOptions<YourDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<Hobby> Hobbies { get; set; }
+        public DbSet<BreakfastFood> BreakfastFoods { get; set; }
+        public DbSet<FavoriteBook> FavoriteBooks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
